19dfee4f908c719e092a17a8e4b9d6bbed5897ea
Mikko Saarinen <el4302@gmail.com>
2023-02-10 13:07:14 +0200

    14 pistettä/20

---
+++ t2/courses.py
@@ -0,0 +1,219 @@
+import os
+import sqlite3
+
+# poistaa tietokannan alussa (kätevä moduulin testailussa)
+os.remove("courses.db")
+
+db = sqlite3.connect("courses.db")
+db.isolation_level = None
+
+# luo tietokantaan tarvittavat taulut
+
+
+def create_tables():
+    db.execute("CREATE TABLE Teachers (\
+        id INTEGER PRIMARY KEY, \
+        name TEXT)")
+    db.execute("CREATE TABLE Courses (\
+        id INTEGER PRIMARY KEY, \
+        name TEXT, \
+        credits INTEGER )")
+    db.execute("CREATE TABLE CourseTeachers (\
+        id INTEGER PRIMARY KEY, \
+        teacher_id REFERENCES Teachers , \
+        course_id REFERENCES Courses)")
+    db.execute("CREATE TABLE Students (\
+        id INTEGER PRIMARY KEY, \
+        name TEXT)")
+    db.execute("CREATE TABLE Credits (\
+        id INTEGER PRIMARY KEY,\
+        student_id REFERENCES Students,\
+        course_id REFERENCES Courses,\
+        date DATE,\
+        grade INTEGER)")
+    db.execute("CREATE TABLE Groups (\
+        id INTEGER PRIMARY KEY, \
+        name TEXT)")
+    db.execute("CREATE TABLE GroupStudents (\
+        id INTEGER PRIMARY KEY, \
+        student_id REFERENCES Students , \
+        group_id REFERENCES Groups)")
+    db.execute("CREATE TABLE GroupTeachers (\
+        id INTEGER PRIMARY KEY, \
+        teacher_id REFERENCES Teachers , \
+        group_id REFERENCES Groups)")
+
+
+# lisää opettajan tietokantaan
+def create_teacher(name):
+    teacher = db.execute("INSERT INTO Teachers (name) VALUES (?)", [name])
+    id = teacher.lastrowid
+    return id
+
+
+# lisää kurssin tietokantaan
+def create_course(name, credits, teacher_ids):
+    #print("create_course("+name+", ", credits, ", ", teacher_ids, ")")
+    course = db.execute(
+        "INSERT INTO Courses (name, credits) VALUES (?, ?)", [name, credits])
+    course_id = course.lastrowid
+    for teacher_id in teacher_ids:
+        db.execute("INSERT INTO CourseTeachers (teacher_id, course_id) VALUES (?,?)", [
+                   teacher_id, course_id])
+    return course_id
+
+
+# lisää opiskelijan tietokantaan
+def create_student(name):
+    student = db.execute("INSERT INTO Students (name) VALUES (?)", [name])
+    id = student.lastrowid
+    return id
+
+
+# antaa opiskelijalle suorituksen kurssista
+def add_credits(student_id, course_id, date, grade):
+    #print("courses.add_credits(", student_id, ",",course_id, ",", date, ",", grade, ")")
+    credit = db.execute("INSERT INTO Credits (student_id, course_id, date, grade)\
+        VALUES (?,?,?,?)", [student_id, course_id, date, grade])
+
+
+# lisää ryhmän tietokantaan
+def create_group(name, teacher_ids, student_ids):
+    #print("create_group(", name, ",", teacher_ids, ",", student_ids, ")")
+    group = db.execute("INSERT INTO Groups (name) VALUES (?)", [name])
+    group_id = group.lastrowid
+    for teacher_id in teacher_ids:
+        db.execute("INSERT INTO GroupTeachers (teacher_id, group_id) VALUES (?,?)", [
+            teacher_id, group_id])
+    for student_id in student_ids:
+        db.execute("INSERT INTO GroupStudents (student_id, group_id) VALUES (?,?)", [
+            student_id, group_id])
+
+
+# hakee kurssit, joissa opettaja opettaa (aakkosjärjestyksessä)
+def courses_by_teacher(teacher_name):
+    #print("courses_by_teacher(", teacher_name, ")")
+    courses = db.execute("SELECT c.name \
+        FROM Courses c, CourseTeachers ct, Teachers t\
+        WHERE t.name = ? AND t.id = ct.teacher_id AND ct.course_id = c.id\
+        ORDER BY c.name ", [teacher_name]).fetchall()
+    return [x[0] for x in courses]
+
+
+# hakee opettajan antamien opintopisteiden määrän
+def credits_by_teacher(teacher_name):
+    #print("credits_by_teacher(", teacher_name, ")")
+    op = db.execute("SELECT COUNT(DISTINCT c.id)*k.credits FROM Teachers t\
+        LEFT JOIN CourseTeachers ct ON ct.teacher_id = t.id \
+        LEFT JOIN Credits c ON ct.course_id = c.course_id\
+        LEFT JOIN Courses k ON k.id = c.course_id\
+        WHERE  t.name = ? ;", [teacher_name]).fetchone()[0]
+    return op
+
+
+# hakee opiskelijan suorittamat kurssit arvosanoineen (aakkosjärjestyksessä)
+def courses_by_student(student_name):
+    # print("courses_by_student(",student_name,")");
+    courses = db.execute("SELECT k.name, c.grade \
+        FROM Courses k, Credits c, Students s\
+        WHERE s.name = ? AND s.id = c.student_id AND c.course_id = k.id\
+        ORDER BY k.name;", [student_name]).fetchall()
+    return courses
+
+
+# hakee tiettynä vuonna saatujen opintopisteiden määrän
+def credits_by_year(year):
+    # print("credits_by_year(",year,")");
+    return db.execute("SELECT COUNT(c.id)*k.credits\
+        FROM Courses k, Credits c\
+        WHERE c.course_id = k.id AND strftime('%Y', c.date) = ? ", [str(year)]).fetchone()[0]
+
+
+# hakee kurssin arvosanojen jakauman (järjestyksessä arvosanat 1-5)
+def grade_distribution(course_name):
+    # print("grade_distribution(",course_name,")")
+    distr = db.execute("SELECT DISTINCT c.grade, count(c.id) \
+        FROM Credits c, Courses k \
+        WHERE k.name = ? AND k.id = c.course_id\
+        GROUP BY c.grade ORDER BY c.grade", [course_name]).fetchall()
+    res = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+    for d in distr:
+        key, value = d
+        res[key] = value
+    return res
+
+
+# hakee listan kursseista (nimi, opettajien määrä, suorittajien määrä) (aakkosjärjestyksessä)
+def course_list():
+    # print("course_list()")
+    courses = db.execute("SELECT DISTINCT c.name, COUNT(DISTINCT ct.id), COUNT(DISTINCT cr.id)\
+        FROM Courses c \
+        LEFT JOIN CourseTeachers ct ON ct.course_id = c.id\
+        LEFT JOIN Credits cr ON cr.course_id=c.id\
+        GROUP BY c.name ORDER BY c.name").fetchall()
+    return courses
+
+
+# hakee listan opettajista kursseineen (aakkosjärjestyksessä opettajat ja kurssit)
+def teacher_list():
+    # print("teacher_list()")
+    teachers = db.execute("SELECT DISTINCT t.name, k.name \
+        FROM Teachers t\
+        LEFT JOIN CourseTeachers ct ON t.id = ct.teacher_id\
+        LEFT JOIN Courses k ON ct.course_id = k.id\
+        GROUP BY t.name, k.name ORDER BY t.name, k.name;").fetchall()
+    res = []
+    ope = ""
+    kurssit = []
+    for teacher in teachers:
+        t, c = teacher
+        if ope == t:
+            kurssit.append(c)
+        else:
+            if ope != "":
+                res.append(((ope, kurssit)))
+            ope = t
+            kurssit = [c]
+    res.append(((ope, kurssit)))
+    return res
+# lopusta puuttuu ('Matti Luukkainen', ['Tietokantojen perusteet'])
+
+# hakee ryhmässä olevat henkilöt (aakkosjärjestyksessä)
+def group_people(group_name):
+    #print("group_people(", group_name, ")")
+    students = db.execute("SELECT s.name\
+        FROM Students s, GroupStudents gs, Groups g\
+        WHERE s.id=gs.student_id AND g.id=gs.group_id AND g.name= ?\
+        ORDER BY s.name", [group_name]).fetchall()
+
+    teachers = db.execute("SELECT t.name\
+        FROM Teachers t, GroupTeachers gt, Groups g\
+        WHERE t.id=gt.teacher_id AND g.id=gt.group_id AND g.name= ?\
+        ORDER BY t.name", [group_name]).fetchall()
+
+    students = [x[0] for x in students]
+    teachers = [x[0] for x in teachers]
+    return(sorted(students+teachers))
+
+
+# hakee ryhmissä saatujen opintopisteiden määrät (aakkosjärjestyksessä)
+def credits_in_groups():
+    #print("credits_in_groups()")
+    groups = db.execute("SELECT g.name, IFNULL(COUNT(DISTINCT cr.id)*c.credits, 0)\
+        FROM Groups g\
+        LEFT JOIN GroupStudents gs ON g.id = gs.group_id \
+        LEFT JOIN Credits cr ON gs.student_id = cr.student_id\
+        LEFT JOIN Courses c ON c.id = cr.course_id\
+        GROUP BY g.name ORDER BY g.name;").fetchall()
+    return groups
+
+
+# hakee ryhmät, joissa on tietty opettaja ja opiskelija (aakkosjärjestyksessä)
+def common_groups(teacher_name, student_name):
+    #print("common_groups(",teacher_name,",",student_name,")")
+    groups = db.execute("SELECT DISTINCT g.name\
+        FROM Groups g, Students s, Teachers t, GroupTeachers gt, GroupStudents gs\
+        WHERE t.name = ? AND s.name = ? \
+        AND gt.teacher_id = t.id AND gs.student_id = s.id AND gt.group_id = gs.group_id AND g.id = gt.group_id\
+        ",[teacher_name, student_name]).fetchall()
+    return [x[0] for x in groups]
---
+++ t2/main.py
@@ -0,0 +1,109 @@
+'''
+import courses
+
+courses.create_tables()
+
+t1 = courses.create_teacher("Erkki Kaila")
+t2 = courses.create_teacher("Antti Laaksonen")
+t3 = courses.create_teacher("Matti Luukkainen")
+t4 = courses.create_teacher("Emilia Oikarinen")
+t5 = courses.create_teacher("Leena Salmela")
+
+c1 = courses.create_course("Laskennan mallit", 5, [t1])
+c2 = courses.create_course("Ohjelmistotuotanto", 5, [t2, t4, t5])
+c3 = courses.create_course("Ohjelmoinnin perusteet", 5, [])
+c4 = courses.create_course("Tietokantojen perusteet", 5, [t3, t5])
+c5 = courses.create_course("Tietokoneen toiminta", 5, [t1])
+
+s1 = courses.create_student("Heikki Lokki")
+s2 = courses.create_student("Liisa Marttinen")
+s3 = courses.create_student("Otto Nurmi")
+s4 = courses.create_student("Esko Ukkonen")
+s5 = courses.create_student("Arto Wikla")
+
+courses.add_credits(s1, c2, "2020-06-01", 5)
+courses.add_credits(s1, c3, "2021-01-08", 3)
+courses.add_credits(s2, c5, "2022-03-23", 2)
+courses.add_credits(s4, c3, "2022-01-27", 4)
+courses.add_credits(s4, c4, "2021-05-05", 4)
+courses.add_credits(s4, c2, "2021-10-03", 5)
+courses.add_credits(s4, c5, "2021-10-04", 5)
+courses.add_credits(s5, c2, "2020-12-24", 1)
+
+courses.create_group("Basic-koodarit", [t2, t3], [s1, s2, s3])
+courses.create_group("Cobol-koodarit", [t1], [s2, s4])
+courses.create_group("Fortran-koodarit", [t1, t2, t3, t4, t5], [s1, s2, s3, s4, s5])
+courses.create_group("PHP-koodarit", [t4, t5], [s3])
+
+print(courses.courses_by_teacher("Leena Salmela"))
+print(courses.credits_by_teacher("Leena Salmela"))
+print(courses.courses_by_student("Esko Ukkonen"))
+
+print(courses.credits_by_year(2020))
+print(courses.credits_by_year(2021))
+print(courses.credits_by_year(2022))
+
+print(courses.grade_distribution("Ohjelmoinnin perusteet"))
+print(courses.grade_distribution("Tietokoneen toiminta"))
+
+print(courses.course_list())
+print(courses.teacher_list())
+
+print(courses.group_people("Basic-koodarit"))
+print(courses.credits_in_groups())
+print(courses.common_groups("Antti Laaksonen", "Otto Nurmi"))
+'''
+
+import courses
+
+courses.create_tables()
+
+t1 = courses.create_teacher("Erkki Kaila")
+t2 = courses.create_teacher("Antti Laaksonen")
+t3 = courses.create_teacher("Matti Luukkainen")
+t4 = courses.create_teacher("Emilia Oikarinen")
+t5 = courses.create_teacher("Leena Salmela")
+
+c1 = courses.create_course("Laskennan mallit", 7, [t1, t3])
+c2 = courses.create_course("Ohjelmistotuotanto", 9, [t1, t2, t5])
+c3 = courses.create_course("Ohjelmoinnin perusteet", 8, [t2, t5])
+c4 = courses.create_course("Tietokantojen perusteet", 4, [t3, t4])
+c5 = courses.create_course("Tietokoneen toiminta", 6, [t5])
+
+s1 = courses.create_student("Heikki Lokki")
+s2 = courses.create_student("Liisa Marttinen")
+s3 = courses.create_student("Otto Nurmi")
+s4 = courses.create_student("Esko Ukkonen")
+s5 = courses.create_student("Arto Wikla")
+
+courses.add_credits(s1, c1, "2020-01-10", 1)
+courses.add_credits(s1, c2, "2021-05-02", 2)
+courses.add_credits(s1, c4, "2021-04-20", 5)
+courses.add_credits(s2, c1, "2021-03-10", 5)
+courses.add_credits(s2, c2, "2022-09-08", 5)
+courses.add_credits(s3, c3, "2022-09-10", 3)
+courses.add_credits(s4, c3, "2022-11-01", 3)
+courses.add_credits(s4, c4, "2020-11-29", 5)
+
+courses.create_group("Basic-koodarit", [t1, t2], [s1, s2, s3, s5])
+courses.create_group("Cobol-koodarit", [t4], [s2, s4, s5])
+courses.create_group("Fortran-koodarit", [], [s5])
+courses.create_group("PHP-koodarit", [t1, t2, t3], [s2, s3, s4, s5])
+
+print(courses.courses_by_teacher("Leena Salmela"))
+print(courses.credits_by_teacher("Leena Salmela"))
+print(courses.courses_by_student("Esko Ukkonen"))
+
+print(courses.credits_by_year(2020))
+print(courses.credits_by_year(2021))
+print(courses.credits_by_year(2022))
+
+print(courses.grade_distribution("Ohjelmoinnin perusteet"))
+print(courses.grade_distribution("Tietokoneen toiminta"))
+
+print(courses.course_list())
+print(courses.teacher_list())
+
+print(courses.group_people("Basic-koodarit"))
+print(courses.credits_in_groups())
+print(courses.common_groups("Antti Laaksonen", "Otto Nurmi"))
